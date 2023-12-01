5d97f13eaa69f717557da42452a84258d7c22d48
PDeger <73216502+PDeger@users.noreply.github.com>
2023-12-01 01:01:48 +0100

    Update Main.java

--- graphProjectBaseline/src/graphProjectBaseline/Main.java
+++ graphProjectBaseline/src/graphProjectBaseline/Main.java
@@ -3,7 +3,6 @@ package graphProjectBaseline;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		//Name der Datei
 		String fileName = "10x10Graph.txt";
