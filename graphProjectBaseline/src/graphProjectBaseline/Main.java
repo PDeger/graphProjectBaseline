55be55db0b4e5efd81e24ede17d6403fa11faca2
PDeger <73216502+PDeger@users.noreply.github.com>
2023-12-01 01:01:31 +0100

    Update Main.java

--- graphProjectBaseline/src/graphProjectBaseline/Main.java
+++ graphProjectBaseline/src/graphProjectBaseline/Main.java
@@ -11,14 +11,10 @@ public class Main {
 		String dir = "weightedDirectionalGraphs";
 		
 		/*
-		 * Akzeptiert "Matrix" oder "List", gibt an, in welcher form der Graph ausgelesen wird. 
-		 * Nur die Matrixdarstellung ist implementiert, die Listendarstellung müsst ihr selbst implementieren,
-		 * wenn ihr sie verwenden wollt. 
-		 * Hilfestellung für die Listendarstellung: Objekte für die Kanten und Knoten liegen schon vor -> Siehe "Node" und "Edge". 
-		 * Es gibt auch ander Möglichekiten für die Implementierung, dies soll nur als Denkanstoß dienen
-		 */
-		String type = "Matrix";
-
+		* Hilfestellung für die Listendarstellung bzw. Adjazenzliste: Objekte für die Kanten und Knoten liegen schon vor -> Siehe "Node" und "Edge". 
+		* Es gibt auch andere Möglichekiten für die Implementierung, dies soll nur als Denkanstoß dienen
+		*/
+		
 		/*
 		 * Den Input Handler könnt ihr einem Objekten oder Funktionen als Parameter mitgeben, 
 		 * um eine Einfache Eingabefunktion zu haben
